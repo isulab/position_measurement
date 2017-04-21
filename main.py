ee04d7e21acc41ac9f1180ca475b0e36cc8b6105
tommy19970714 <toshiki19970714@gmail.com>
2017-04-21 15:42:49 +0900

    [modify] arrangement after merge

--- main.py
+++ main.py
@@ -9,21 +9,16 @@ import math
 import csv
 import os
 
-#/dev/tty.wchusbserial1410
+
+usleep = lambda x: sleep(x/1000000.0)
+
 SERIAL_PORT_NAME = 'COM3' if os.name == 'nt' else '/dev/tty.wchusbserial1420'
 SERIAL_BAUD_RATE = 38400
+MAX_INCLINATION = 15
 
-FREQUENT = 200
-
-class Control:
-
-    def makeSin(self,time):
-        return math.sin(2*math.pi*time*FREQUENT)
-
-    def getOutStr(self,time):
-        s = self.makeSin(time)
-        return str(s)+str(s)+str(s)
-
+'''
+CSV書き込みを扱うクラス
+'''
 class CSVWriter:
 
     def __init__(self, filename):
@@ -34,20 +29,30 @@ class CSVWriter:
     def writeRow(self, time, axis1, axis2, axis3):
         self.writer.writerow([time, axis1, axis2, axis3])
 
+    def writeRowText(self, time, text):
+        self.writer.writerow([time, text])
+
     def __del__(self):
         self.file.close()
 
-
+'''
+時間を管理するクラス
+'''
 class TimeMeasure:
 
     def __init__(self):
         self.starttime = time.time()
 
-    def getTime(self):
+    def getTimeString(self):
         return str(time.time() - self.starttime)
 
+    def getTimeFloat(self):
+        return time.time() - self.starttime
 
-class Control():
+'''
+制御値を計算するクラス
+'''
+class Control:
     def __init__(self, frequent, amplitude, raising):
         """
         :param frequent: 周波数[Hz] 
@@ -56,7 +61,7 @@ class Control():
         """
 
         self.frequent = frequent
-        self.max_inclination = max_inclination
+        self.max_inclination = MAX_INCLINATION
         self.amplitude = amplitude
         self.raising = raising
         self.MAX_TARGET_VALUE = 4000
@@ -65,24 +70,28 @@ class Control():
         target_value = self.raising + math.sin(time * self.frequent / 2000 * math.pi) * self.amplitude
         if 0 > target_value or target_value > self.MAX_TARGET_VALUE :
             target_value = math.floor(target_value / self.MAX_TARGET_VALUE) * self.MAX_TARGET_VALUE
-        return hex(target_value)
+        return target_value
 
     def make_square(self, time):
-        target_value = self.raising +  self.amplitude if time / 1000 / frequent < frequent / 2 else self.amplitude
+        target_value = self.raising +  self.amplitude if time / 1000 / self.frequent < self.frequent / 2 else self.amplitude
         if 0 > target_value or target_value > self.MAX_TARGET_VALUE :
-            target_value = math.floor(value / self.MAX_TARGET_VALUE) * self.MAX_TARGET_VALUE
-        return hex(target_value)
+            target_value = math.floor(target_value / self.MAX_TARGET_VALUE) * self.MAX_TARGET_VALUE
+        return target_value
 
     def get_out_sin_str(self, time1, time2, time3):
-        return  self.make_square(time1) + self.make_square(time2) + self.make_square(time1)
+        return '{0:04x}'.format(self.make_square(time1)) + '{0:04x}'.format(self.make_square(time2)) + '{0:04x}'.format(self.make_square(time3))
 
     def get_out_square_str(self, time1, time2, time3):
-        return self.make_square(time1) + self.make_square(time2) + self.make_square(time1)
+        return '{0:04x}'.format(self.make_square(time1)) + '{0:04x}'.format(self.make_square(time2)) + '{0:04x}'.format(self.make_square(time3))
 
+'''
+マイコンとのシリアル通信のクラス
+'''
 class SASerial:
 
     def __init__(self,port,baudrate):
         self.ser = serial.serial_for_url(port, baudrate=baudrate, timeout=5)
+        self.writer = CSVWriter("SendControlData.csv")
 
     class PrintLines(LineReader):
         TERMINATOR = b'\n'
@@ -91,13 +100,12 @@ class SASerial:
             super(SASerial.PrintLines, self).connection_made(transport)
             sys.stdout.write('port opened\n')
             self.transport.serial.reset_input_buffer()
-            self.writer = CSVWriter("data.csv")
+            self.writer = CSVWriter("RecievePosisionData.csv")
 
             # def data_received(self, data):
             # print('data received', repr(data))
 
         def handle_line(self, data):
-            # print('handle_line')
             sys.stdout.write('line received: {}\n'.format(repr(data)))
             recieve = repr(data)
             if len(recieve) >= 14:
@@ -105,8 +113,7 @@ class SASerial:
                 axis2 = int(recieve[5:9], 16)
                 axis3 = int(recieve[9:13], 16)
                 global timer
-                self.writer.writeRow(timer.getTime(), axis1, axis2, axis3)
-
+                self.writer.writeRow(timer.getTimeString(), axis1, axis2, axis3)
 
         def connection_lost(self, exc):
             if exc:
@@ -115,12 +122,24 @@ class SASerial:
 
     def main(self):
         with ReaderThread(self.ser, self.PrintLines) as protocol:
-            control = Control()
+            control = Control(0.3,1,0)
             sleep(1)
 
-            for time in range(0,100):
-                protocol.write_line("AAAABBBBCCCC")
+            for roop in range(0,100):
+                global timer
+                now = timer.getTimeFloat()
+                axis1 = control.make_sin(now)
+                axis2 = control.make_sin(now)
+                axis3 = control.make_sin(now)
+                outvalue = control.get_out_sin_str(now,now,now)
+                protocol.write_line(outvalue)
+                self.writer.writeRow(timer.getTimeString(), axis1, axis2, axis3)
+                usleep(100)
 
-timer = TimeMeasure()
-ser = SASerial(SERIAL_PORT_NAME,SERIAL_BAUD_RATE)
-ser.main()
+'''
+メイン
+'''
+if __name__ == '__main__':
+    timer = TimeMeasure()
+    ser = SASerial(SERIAL_PORT_NAME,SERIAL_BAUD_RATE)
+    ser.main()
